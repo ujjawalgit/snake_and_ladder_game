1eb6a7faafecc383bd8a8bd886419793b62a3e5f
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-06-24 17:45:17 +0530

    checks for a Option They are No Play, Ladder or Snake

---
+++ SnakeAndLadder.java
@@ -0,0 +1,21 @@
+﻿public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+
+        final int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        int position = 0, option = 0, die = 0;
+
+        die = (int) Math.floor(Math.random() * 1000) % 6 + 1;
+        option = (int) Math.floor(Math.random() * 10) % 3;
+        switch (option) {
+
+            case LADDER:
+                position += die;
+                break;
+
+            case SNAKE:
+                position += die;
+                break;
+        }
+    }
+}
