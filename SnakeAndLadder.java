734991fe872bf9788797b00a1026c0bf28e5fd1a
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-06-24 17:56:54 +0530

    Ensure the player gets to exact winning position 100

---
+++ SnakeAndLadder.java
@@ -0,0 +1,37 @@
+﻿public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+
+        final int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        int position = 0, option = 0, die = 0;
+
+        while (true) {
+            die = (int) Math.floor(Math.random() * 1000) % 6 + 1;
+            option = (int) Math.floor(Math.random() * 10) % 3;
+            switch (option) {
+
+                case LADDER:
+                    if ((position + die) > 100)) {
+                position = position;
+                break;
+            } else {
+                position += die;
+                break;
+            }
+
+            case SNAKE:
+                if ((position - die) < 0) {
+                    position = 0;
+                }
+                break;
+                else {
+                    position -= die;
+                }
+
+        }
+        if (position == 100)
+            break;
+
+    }
+}
+}
