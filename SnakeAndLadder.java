a292ea6ce668b4aaadd471145828c92760586604
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-06-24 17:20:32 +0530

    start at position 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,6 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        final int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        int position = 0, option = 0, die = 0;
+    }
+}
