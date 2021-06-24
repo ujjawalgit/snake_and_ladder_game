ab23d446c222411722fe80bb7f110d4bbd49260c
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-06-24 17:50:49 +0530

    Repeat till the Player reaches the winning position 100

---
+++ SnakeAndLadder.java
@@ -0,0 +1,32 @@
+﻿public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+
+        final int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        int position = 0, option = 0, die = 0;
+
+        while (true) {
+            die = (int) Math.floor(Math.random() * 1000) % 6 + 1;
+            option = (int) Math.floor(Math.random() * 10) % 3;
+            switch (option) {
+
+                case LADDER:
+                    position += die;
+                    break;
+
+                case SNAKE:
+                    if ((position - die) < 0) {
+                        position = 0;
+                    }
+                    break;
+                    else {
+                        position -= die;
+                    }
+
+            }
+            if (position == 100)
+                break;
+
+        }
+    }
+}
